47c3e382fe56d44df4f8f8f8b37915700d3e5511
seungeun <grandse@naver.com>
2025-01-15 14:07:56 +0900

    2025.01.15 도커 배포 오류 수정 - AppConfig.java - application.properties - gitignore

--- backend/MATE/src/main/java/com/example/MATE/config/Appconfig.java
+++ backend/MATE/src/main/java/com/example/MATE/config/Appconfig.java
@@ -1,18 +1,23 @@
 package com.example.MATE.config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Appconfig {
 
-    public static String ServerPort;
+    @Getter
+    private static String serverDomain;
 
-    @Value("${server.port}")
-    public void setServerPort(String server_port) {
-        ServerPort = server_port;
+    @Value("${domain}")
+    public void setServerDomain(String serverDomain) {
+        Appconfig.serverDomain = serverDomain;
+
+        // 디버깅 로그
         System.out.println("====================================");
-        System.out.println("===> Server PORT : " + ServerPort);
+        System.out.println("===> Server Domain : " + Appconfig.serverDomain);
         System.out.println("====================================");
     }
+
 }
