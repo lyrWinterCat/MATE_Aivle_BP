1943d76cd4b3e6a53751ad80bf11e7aec71130b8
WinterCat <dldulfla27@gmail.com>
2025-01-21 17:12:47 +0900

    delete : 쓸데없는 코드 삭제
    저거 동작 안해요...

--- screenshare_java/src/main/java/com/example/screenshare/model/ScreenData.java
+++
@@ -1,9 +0,0 @@
-package com.example.screenshare.model;
-
-import lombok.Data;
-
-@Data
-public class ScreenData {
-    private String type;    // 'offer', 'answer', 'candidate'
-    private Object payload;
-}
