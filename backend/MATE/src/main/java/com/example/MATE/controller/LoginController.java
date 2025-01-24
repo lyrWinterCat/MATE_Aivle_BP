7f25547052e198103002fc9b26199f0b3ff08a63
SUHYEON KO <h6103612@gmail.com>
2025-01-25 02:30:36 +0900

    보랭이 index, aboutus, singIn, 회원가입 이식

--- backend/MATE/src/main/java/com/example/MATE/controller/LoginController.java
+++ backend/MATE/src/main/java/com/example/MATE/controller/LoginController.java
@@ -34,7 +34,7 @@ public class LoginController {
 
     //메인페이지(로그인페이지)이동
     @GetMapping("/")
-     public String index(){ return "login/index"; }
+    public String index(){ return "login/index"; }
 
     //조원 소개페이지
     @GetMapping("/aboutus")
@@ -42,7 +42,6 @@ public class LoginController {
         System.out.println(">>> [LoginController] /aboutus 요청 처리");
         return "login/aboutus"; }
 
-//    @GetMapping("/contact")
 
     //로그인 페이지이동
     @GetMapping("/signIn")
