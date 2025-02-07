2fab899ce5a01c46ebf38096ce44377deca4ba3f
SUHYEON KO <h6103612@gmail.com>
2025-02-07 16:57:18 +0900

    안끝난 회의만 가져오도록 MeetingRepository.findMeetingIdsByUserId 수정

--- backend/MATE/src/main/java/com/example/MATE/repository/MeetingRepository.java
+++ backend/MATE/src/main/java/com/example/MATE/repository/MeetingRepository.java
@@ -15,9 +15,21 @@ public interface MeetingRepository extends JpaRepository<Meeting, Integer> {
     // Meeting 정보를 불러오기 위한 query derived method 작성
     @Query(value = "SELECT * FROM meeting ORDER BY start_time DESC", nativeQuery = true) // JPQL 이 아닌 native query 사용
     List<Meeting> findAllMeetings();
+
     // 유저 ID 를 받아 해당 유저가 참여한 모든 미팅 ID 를 반환
-    @Query(value = "SELECT meeting_id FROM meeting_participant WHERE user_id = ?1", nativeQuery = true)
+    // 안끝난 회의에 대해서만 id 를 가져옴
+    @Query(
+            """
+            SELECT DISTINCT m.id
+            FROM MeetingParticipant mp
+            JOIN mp.meeting m
+            WHERE mp.user.id = ?1
+            AND m.endTime IS NULL
+            """
+    )
     List<Integer> findMeetingIdsByUserId(Integer userId);
+
+
 
     // 특정 사용자 userId를 가진 MeetingParticipant가 속한 Meeting을 페이징
     Page<Meeting> findByMeetingParticipants_User_UserId(Integer userId, Pageable pageable);
